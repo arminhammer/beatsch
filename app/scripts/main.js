667967013c23fe62478e37146baba1f2fcd8c59a
arminhammer <arminhammer@gmail.com>
2015-07-26 18:59:43 -0400

    Adding some elements to main.js.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,9 +4,9 @@
 var MainPage = require('./views/main.page');
 var VideoModel = require('./models/video.model');
 
-var video1 = new VideoModel({ title: 'Video 1', description: 'This is Video 1', score: 125 });
-var video2 = new VideoModel({ title: 'Video 2', description: 'This is Video 2', score: 475 });
-var video3 = new VideoModel({ title: 'Video 3', description: 'This is Video 3', score: 300 });
+var video1 = new VideoModel({ title: 'Video 1', description: 'This is Video 1', score: 125, youtubeId: 'YqeW9_5kURI' });
+var video2 = new VideoModel({ title: 'Video 2', description: 'This is Video 2', score: 475, youtubeId: 'g_uoH6hJilc' });
+var video3 = new VideoModel({ title: 'Video 3', description: 'This is Video 3', score: 300, youtubeId: 'QcIy9NiNbmo' });
 
 var playList = [];
 playList.push(video1);
